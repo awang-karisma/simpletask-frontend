83223abe27267223f56d662541f2cc15c9a07f83
awang-karisma <4899660+awang-karisma@users.noreply.github.com>
2022-08-07 16:20:36 +0000

    Deploying to gh-pages from @ awang-karisma/simpletask-frontend@02834099ca7ec03e98c255e68ae3f90571421035 🚀

--- server/nodes/2.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const index = 2;
-export const file = '_app/immutable/pages/index.svelte-9bf6359f.js';
-export const imports = ["_app/immutable/pages/index.svelte-9bf6359f.js","_app/immutable/chunks/index-de80af4f.js","_app/immutable/chunks/index-c19beea7.js"];
-export const stylesheets = [];
